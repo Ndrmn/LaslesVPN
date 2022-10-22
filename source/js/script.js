b5bce070278fac9a2d7cf254beceb74c0f75020e
Ndrmn <andrkult@gmail.com>
2022-10-22 20:44:01 +0200

    22.10 update

---
+++ source/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('.customers__cards').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      dots: true,
+      appendArrows: $('.buttons__right'),
+      prevArrow: $('.left'),
+      nextArrow: $('.right'),
+      appendDots: $('.buttons__left'),
+      variableWidth: true,
+      adaptiveHeight: true
+  });
+
+});
+
